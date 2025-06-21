cbf38c9604847e6acbf9b1b051661376a910836d
Thang Nguyen <thangconconhung@gmail.com>
2025-06-21 11:52:00 +0700

    vid39

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,9 +10,26 @@ const getHoiIt = (req, res) => {
 };
 
 const postCreateuser = (req, res) => {
-  console.log("req.body", req.body)
-  res.send("created a user");
-}
+  let email = req.body.email;
+  let name = req.body.name;
+  let city = req.body.city;
+  // let { email, name, city } = req.body;
+
+  connection.query(
+    `INSERT INTO users  (email, name, city)
+    VALUES (?, ?, ?)`,
+    [email, name, city],
+    function (err, results) {
+      if (err) {
+        console.error('Lỗi khi thêm user:', err);
+        return res.status(500).send('Lỗi server khi tạo user');
+      }
+
+      console.log('Kết quả:', results);
+      res.send('Tạo user thành công');
+    }
+  );
+};
 
 module.exports = {
   getHomePage,
